0500cfd444b0d772f0b47dc0f656574def7ba3ad
Артём <lada77741@gmail.com>
2024-11-05 20:58:03 +0300

    bug fix

--- module_3_5.py
+++ module_3_5.py
@@ -3,9 +3,11 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
+    elif first == 0:
+        return 1
     else:
         return first
 
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(4020)
 print(result)
