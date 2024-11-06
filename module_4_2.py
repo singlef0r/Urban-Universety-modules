a6132084b9e6ed5f453735071bfd6de904a55fb8
Артём <lada77741@gmail.com>
2024-11-06 20:37:12 +0300

    install module in git

--- module_4_2.py
+++ module_4_2.py
@@ -1,8 +1,10 @@
-# innnr_function()
-
 def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
-        inner_function()
 
-# innnr_function()
+
+    inner_function()
+
+
+test_function()
+innnr_function()
